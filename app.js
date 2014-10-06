16836f2d3db868a53749670e2b39038d55a5e5a5
janekf <jan.fellien@devcrowd.de>
2014-10-06 09:49:06 +0200

    move db server mezhodes to a seperated file, so I can define more db server for tests

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 
 var restify = require('restify');
+var db_server = require('./postgresql-db-server.js');
 
-var pg = require("pg");
 var connectionString = "pg://OpenSpaceGuest:OpenSpace2014@localhost:5432/OpenSpace";
 
 
@@ -19,7 +19,6 @@ server
 // Define Routes
 
 server.post('/init/:account', createEventStoreIfNotExists);
-server.post('/store', unableToStoreEvent);
 server.post('/store/:account', storeEvent);
 server.get('/events/:account', readAllAccountEvents);
 server.get('/events/:account/:entityId', readAllEntityEvents);
@@ -42,42 +41,14 @@ function createEventStoreIfNotExists(req, res, next) {
 
     console.log('Account %s will create its own event store', account);
 
-    pg.connect(connectionString, function(err, dbClient, done){
+    db_server.createEventStoreFor(connectionString, account, function(err, result){
 
-        if(err) {
-            return console.error('could not connect to postgres', err);
+        if(err){
+            res.send(500, err);
         }
 
-        var createIfNotExistsQuery = 'CREATE TABLE IF NOT EXISTS '
-            + account
-            + '('
-            + 'rowId bigserial primary key,'
-            + 'entityId varchar(64), '
-            + 'eventId varchar(64), '
-            + 'eventType varchar(128), '
-            + 'eventData varchar(512), '
-            + 'eventTime varchar(64))';
-
-        dbClient.query(createIfNotExistsQuery,
-            function(err, result) {
-
-                done();
-
-                if(err) {
-                    return console.error('error running CREATE TABLE query', err);
-                }
-            });
-
+        res.send(200)
     });
-
-    res.send(200)
-}
-
-function unableToStoreEvent(req, res, next) {
-
-    console.log('Account must be supplied')
-
-    return next(new restify.InvalidArgumentError('Account must be supplied'))
 }
 
 function storeEvent(req, res, next) {
@@ -95,42 +66,15 @@ function storeEvent(req, res, next) {
 
     console.log('Event with Id : %s will write for Account : %s ', parameters.EventId, account);
 
-    pg.connect(connectionString,
-        function(err, dbClient, done){
+    db_server.storeEvent(connectionString, account, parameters, function(err, result){
 
-            if(err) {
-                return console.error('could not connect to postgres', err);
-            }
+        if(err){
+            res.send(500, err);
+        }
 
-            var saveEventQuery =
-                'INSERT INTO '
-                + account
-                + '(entityId, eventId, eventType, eventData, eventTime) '
-                + 'values($1, $2, $3, $4, $5)';
+        res.send(200);
 
-            dbClient.query(saveEventQuery,
-                [
-                    parameters.EntityId,
-                    parameters.EventId,
-                    parameters.EventType,
-                    parameters.EventData,
-                    parameters.TimeStamp
-                ],
-                function(err, result) {
-
-                    done();
-
-                    if(err) {
-                        return console.error('error running INSERT query', err);
-                    }
-
-                    console.log('Event for Account %s has been written', account);
-
-                    res.send(200, result);
-
-                });
-
-        });
+    });
 }
 
 function readAllAccountEvents(req,res, next){
@@ -147,33 +91,14 @@ function readAllAccountEvents(req,res, next){
 
     console.log('Account %s will read all events', account);
 
-    var dbClient = new pg.Client(connectionString);
-
-    dbClient.connect(function(err){
-
-        if(err) {
-            return console.error('could not connect to postgres', err);
+    db_server.readAllAccountEvents(connectionString, account, function(err, result){
+        if(err){
+            res.send(500, err);
         }
 
-        var selectAllEventsQuery = 'SELECT entityId, eventId, eventType, eventData, eventTime FROM ' + account;
-
-        dbClient.query(selectAllEventsQuery, function(err, result){
-
-            if(err) {
-                res.send(200, err);
-                return console.error('error running SELECT ALL query', err);
-            }
-
-            console.log('Account %s has read %s events', account, result.rows.length);
-
-            res.contentType = 'json';
-            res.send(200, result.rows);
-
-            dbClient.end();
-
-        });
-
-    })
+        res.contentType = 'json';
+        res.send(200, result.rows);
+    });
 
 }
 
@@ -192,37 +117,14 @@ function readAllEntityEvents(req,res, next){
 
     console.log('Account %s will read all events for entity %s', account, entityId);
 
-    var dbClient = new pg.Client(connectionString);
-
-    dbClient.connect(function(err){
-
-        if(err) {
-            return console.error('could not connect to postgres', err);
+    db_server.readAllEntityEvents(connectionString, account, entityId, function(err,result){
+        if(err){
+            res.send(500, err);
         }
 
-        var selectAllEventsQuery =
-                'SELECT entityId, eventId, eventType, eventData, eventTime FROM '
-                + account + ' '
-                + 'WHERE entityid=$1';
-
-        dbClient.query(selectAllEventsQuery, [entityId], function(err, result){
-
-            if(err) {
-                res.send(200, err);
-                return console.error('error running SELECT BY ENTITY query', err);
-            }
-
-            console.log('Account %s has read %s events for entity %s', account, result.rows.length, entityId);
-
-            res.contentType = 'json';
-            res.send(200, result.rows);
-
-            dbClient.end();
-
-        });
-
-    });
-
+        res.contentType = 'json';
+        res.send(200, result.rows);
+    })
 }
 
 function removeEventStoreForAnAccount(req, res, next){
@@ -238,32 +140,13 @@ function removeEventStoreForAnAccount(req, res, next){
 
     console.log('Account %s will remove the event store', account);
 
-    var dbClient = new pg.Client(connectionString);
+    db_server.removeEventStore(connectionString,account, function(err, result){
 
-    dbClient.connect(function(err){
-
-        if(err) {
-            return console.error('could not connect to postgres', err);
+        if(err){
+            res.send(500, err);
         }
 
-        var removeEventStoreQuery =
-            'DROP TABLE IF EXISTS ' + account;
-
-        dbClient.query(removeEventStoreQuery, function(err, result){
-
-            if(err) {
-                return console.error('error running DROP TABLE query', err);
-            }
-
-            console.log('Account %s have removed its event store', account);
-
-            res.contentType = 'json';
-            res.send(200, '{"Message":"EventStore removed"}');
-
-            dbClient.end();
-
-        });
-
+        res.contentType = 'json';
+        res.send(200, '{"Message":"EventStore removed"}');
     });
-
 }
